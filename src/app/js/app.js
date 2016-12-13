5650491b433c853e6732eeb625908100e6025ca0
Kagu-chan <k.boese@kagu-chan.de>
2016-12-14 00:11:33 +0100

    Restructured app and added angular #135

--- src/app/js/app.js
+++
@@ -1,6 +0,0 @@
-require('app-module-path').addPath(__dirname);
-
-const Main = require('modules/main');
-
-Main.configure();
-Main.run();
---
+++ src/app/ui/controllers/main/index.js
@@ -0,0 +1,4 @@
+angular.module('LuaSB', [])
+  .controller('mainController', ($scope) => {
+    $scope.title = 'LuaSB';
+  });
